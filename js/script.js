22bb09a0713ba4a1e70a4c7bc664c1d45d2a0aad
Dominik Skorupski <dominik.skorupski1@gmail.com>
2020-04-02 16:14:10 +0200

    added eslint to project and fixed script.js code

--- js/script.js
+++ js/script.js
@@ -2,14 +2,14 @@
 
 
 const printMessage = function(msg){
-	let div = document.createElement('div');
-	div.innerHTML = msg;
-	document.getElementById('messages').appendChild(div);
-}
+  let div = document.createElement('div');
+  div.innerHTML = msg;
+  document.getElementById('messages').appendChild(div);
+};
 
 const clearMessages = function(){
-	document.getElementById('messages').innerHTML = '';
-}
+  document.getElementById('messages').innerHTML = '';
+};
 
 // THE GAME
 
@@ -17,15 +17,15 @@ const playGame = function(playerMove){
 
   clearMessages();
 
-	const getMoveName = function(argMoveId){
+  const getMoveName = function(argMoveId){
     if(argMoveId == 1) return 'kamień';
     if(argMoveId == 2) return 'papier';
     if(argMoveId == 3) return 'nożyce';
-  }
+  };
 
   const displayResult = function(argComputerMove, argPlayerMove){
 
-		if (argComputerMove == 'kamień' && argPlayerMove == 'papier'){
+    if (argComputerMove == 'kamień' && argPlayerMove == 'papier'){
       return 'Wygrywasz';
     } else if (argComputerMove == 'nożyce' && argPlayerMove == 'kamień'){
       return 'Wygrywasz';
@@ -36,28 +36,28 @@ const playGame = function(playerMove){
     } else {
       return 'Przegrywasz';
     }
-  }
+  };
 
   const randomNumber = Math.floor(Math.random() * 3 + 1),
- 				computerMove = getMoveName(randomNumber);
+    computerMove = getMoveName(randomNumber);
 
   printMessage('Mój ruch to: ' + computerMove);
   printMessage('Twój ruch to: ' + playerMove);
 
   printMessage(displayResult(computerMove, playerMove));
   
-}
+};
 
 document.getElementById('button-rock').addEventListener('click', function() {
   playGame('kamień');   
 });
 
 document.getElementById('button-paper').addEventListener('click', function() {
-	playGame('papier');
+  playGame('papier');
 });
 
 document.getElementById('button-scissors').addEventListener('click', function() {
-	playGame('nożyce');
+  playGame('nożyce');
 });
 
 
